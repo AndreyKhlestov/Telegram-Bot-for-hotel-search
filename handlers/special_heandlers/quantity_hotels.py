d8d7ab64875a6f6445f711dcf871174b84b5c98f
R2-D2 <andrei-h-74@mail.ru>
2022-06-06 00:53:34 +0300

    Задокументировал функции в сценарии ввода количества отелей

--- handlers/special_heandlers/quantity_hotels.py
+++ handlers/special_heandlers/quantity_hotels.py
@@ -1,11 +1,12 @@
 from loader import bot
 from states.user_states import UserState
-from telebot.types import Message,ReplyKeyboardRemove
+from telebot.types import Message, ReplyKeyboardRemove
 from utils.data import set_data
 from keyboards.reply.default_reply_keyboard import reply_keyboards
 
 
 def start_quantity_hotels(user_id: int, chat_id: int) -> None:
+    """Начало процедуры ввода количество отелей, которые необходимо вывести в результате"""
     bot.set_state(user_id, UserState.quantity_hotels, chat_id)
     bot.send_message(user_id, 'Введите количество отелей, которые необходимо вывести в результате (не больше 25)',
                               reply_markup=reply_keyboards(['5', '7', '10', '15', '20', '25'], 3))
@@ -13,6 +14,7 @@ def start_quantity_hotels(user_id: int, chat_id: int) -> None:
 
 @bot.message_handler(state=UserState.quantity_hotels)
 def quantity_hotels(message: Message) -> None:
+    """Функция для выполнения действий после ввода пользователя количества отелей"""
     from handlers.special_heandlers.confirm import confirm
     if message.text.isdigit():
         if 0 < int(message.text) <= 25:
