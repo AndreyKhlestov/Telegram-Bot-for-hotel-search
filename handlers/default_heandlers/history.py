46fc82f38280e61c88ddd2f8f33308bfb86b284c
Андрей <andrei-h-74@mail.ru>
2022-07-03 20:33:43 +0300

    изменил команду history - теперь выдает последний запрос по новой базе данных

--- handlers/default_heandlers/history.py
+++ handlers/default_heandlers/history.py
@@ -1,7 +1,7 @@
 from telebot.types import Message
 from loader import bot
 from loguru import logger
-from database.models import *
+from database.models import HotelRequest, Hotel
 from handlers.special_heandlers.finish_work import finish_work
 import time
 
@@ -10,11 +10,19 @@ import time
 @logger.catch()
 def bot_history(message: Message):
     logger.info('Запущена команда "history"')
-    data = User.select().where(User.user_id == message.from_user.id)
-    for i_data in data:
-        bot.send_message(message.from_user.id, f'✍ Команда: {i_data.command}\n'
-                                               f'📆 Дата: {i_data.date}\n'
-                                               f'🕓 Время: {i_data.time}\n'
-                                               f'📝 Найденные отели: \n{i_data.name_hotels}')
+
+    # Вывод последнего запроса
+    my_request = HotelRequest.select().where(HotelRequest.user_id == message.from_user.id).order_by(-HotelRequest.date)\
+        .get()
+    text = f'🔍 Команда пользователя: {my_request.command}\n\n' \
+           f'📅 Дата: {my_request.date.split()[0]}\n' \
+           f'🕑 Время: {my_request.date.split()[1]}\n\n' \
+           f'{my_request.main_info}'
+
+    bot.send_message(message.from_user.id, text)
+
+    hotels = Hotel.select().where(Hotel.request_id == my_request.id).order_by(Hotel.num_queue)
+    for i_hotel in hotels:
+        bot.send_message(message.from_user.id, i_hotel.hotel_info)
         time.sleep(1.1)
     finish_work(message.from_user.id, message.chat.id)
